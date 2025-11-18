900530371ce89879bdfa7b2aa996a2ca37ff9c43
pathan-kaif-khan <110097550+pathan-kaif-khan@users.noreply.github.com>
2025-11-18 22:06:05 +0530

    Add files via upload

---
+++ Data_Warehouse_Fundamentals/HandsOnExperienceProjects/Cubes, Rollups, Grouping Sets and Materialized Views/operation on data.sql
@@ -0,0 +1,55 @@
+
+--Grouping sets
+select year,category, sum(billedamount) as totalbilledamount
+from "FactBilling"
+left join "DimCustomer"
+on "FactBilling".customerid = "DimCustomer".customerid
+left join "DimMonth"
+on "FactBilling".monthid="DimMonth".monthid
+group by grouping sets(year,category);
+
+
+--Rollup
+select year,category, sum(billedamount) as totalbilledamount
+from "FactBilling"
+left join "DimCustomer"
+on "FactBilling".customerid = "DimCustomer".customerid
+left join "DimMonth"
+on "FactBilling".monthid="DimMonth".monthid
+group by rollup(year,category)
+order by year, category;
+
+
+--Cube
+select year,category, sum(billedamount) as totalbilledamount
+from "FactBilling"
+left join "DimCustomer"
+on "FactBilling".customerid = "DimCustomer".customerid
+left join "DimMonth"
+on "FactBilling".monthid="DimMonth".monthid
+group by cube(year,category)
+order by year, category;
+
+
+--Materialized Query Table(Materialized View)
+CREATE MATERIALIZED VIEW countrystats (country, year, totalbilledamount) AS
+(select country, year, sum(billedamount)
+from "FactBilling"
+left join "DimCustomer"
+on "FactBilling".customerid = "DimCustomer".customerid
+left join "DimMonth"
+on "FactBilling".monthid="DimMonth".monthid
+group by country,year);
+
+select year, quartername, sum(billedamount) as totalbilledamount
+from "FactBilling"
+left join "DimCustomer"
+on "FactBilling".customerid = "DimCustomer".customerid
+left join "DimMonth"
+on "FactBilling".monthid="DimMonth".monthid
+group by grouping sets(year, quartername);
+
+REFRESH MATERIALIZED VIEW countrystats;
+
+select * from countrystats;
+
